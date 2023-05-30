706e11ebb0def28a51ca8b8abef215a8f1a5bc94
Artiom Kalugin <a.o.kalugin@gmail.com>
2023-05-30 12:27:50 +0100

    Express setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const main = () => {
+  const app = express();
+
+  app.get("/", (_, res) => {
+    res.send("Hello World!");
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+  });
+};
+
+main();
